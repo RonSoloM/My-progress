667fa0828d0f445cd618f4d4b393f89abf093ea4
Ron Solo <ronsolo929@gmail.com>
2020-09-20 09:23:41 +0300

    adding Section 5 files(completed)

--- python-code/Secation  5/Sequences/number_lists.py
+++ python-code/Secation  5/Sequences/number_lists.py
@@ -1,14 +1,12 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
+numbers = [even, odd]
+print(numbers)
 
-print()
-print(len(even))
-print(len(odd))
+for number_list in numbers:
+    print(number_list)
 
-print()
-print("mississippi".count("issi"))
+    for value in number_list:
+        print(value)
