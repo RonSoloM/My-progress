1bb04d22ce78446a099e145cbacbf2fd6d4950ea
Ron Solo <ronsolo929@gmail.com>
2020-09-26 09:02:24 +0300

    Section 6

--- python-code/Randome Files/Homework Test.py
+++ python-code/Randome Files/Homework Test.py
@@ -1,35 +1,9 @@
-albums = [
-    ("welcom to my Nightmare ", "Alive Cooper", 1975,
-     [
-         (1, "Welcome to my Nightmare"),
-         (2, "Devil's Food"),
-         (3, "The Black Widow"),
-         (4, "Some Folks"),
-         (5, "Only Women Bleed"),
-     ]
-     ),
-    ("Bad Company", "Bad Company",1974,
-     [
-         (1, "Can't Get Enough"),
-         (2, "Rock Steady"),
-         (3, "Ready For Love"),
-         (4, "Don't Let Me Down"),
-         (5, "Bad Company"),
-         (6, "The Way I Choose"),
-         (7, "Movin' On"),
-         (8, "Seagull"),
+def is_palindrome(string):
+    return string[::-1].casefold() == string.casefold()
 
-     ]
-     ),
-    ("Nightflight","budgie",1981,
-     [
-         (1, "Pulling the rug"),
-         (2, "Psycho"),
-         (3, "Mayhem"),
-         (4, "Kentish Town Waktz"),
-     ]
-     ),
-]
 
-for name, atrist, year, songs in albums:
-    print("Album: {}, Atrists: {},Year: {},Songs: {}".format(name, atrist, year, songs))
+word = str(input("Please enter a word to check: "))
+if is_palindrome(word):
+    print(True)
+else:
+    print(False)
