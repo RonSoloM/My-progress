d14f12300535b39bb628bace65c4fb08939fb0c8
RonSoloM <ronsolo929@gmail.com>
2021-01-24 11:52:33 +0200

    'hello'

--- python-code/Randome Files/adventure.py
+++ python-code/Randome Files/adventure.py
@@ -1,4 +1,8 @@
-x = input()
-y = input()
+carsBrand = ['Honda', 'Black', 'BMW', 'Ferrari']
 
-print(x)
+Picker = input()
+
+if Picker in carsBrand:
+    print("We got this car and it's number {} in our list".format(len(Picker)))
+else:
+    print("We don't have this car")
